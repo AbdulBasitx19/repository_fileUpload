dbf63a27d0d7e6249590b87529304ddfb54e2db8
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-15 03:27:39 +0500

    routes for Document Uploading, Added.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocumentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DocumentController::class, 'index'])->name('documents.index');
+Route::post('/documents', [DocumentController::class, 'store'])->name('documents.store');
+Route::get('/documents/{id}/download', [DocumentController::class, 'download'])->name('documents.download');
+Route::delete('/documents/{id}', [DocumentController::class, 'destroy'])->name('documents.destroy');
