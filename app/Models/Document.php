1dabb55219cd8900ca594545a65cceb2a3841905
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-15 02:51:34 +0500

    feat: create migration and model for Document(file uploading)

---
+++ app/Models/Document.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Document extends Model
+{
+    //
+    protected $fillable =[
+        'title',
+        'orignal_name',
+        'file_path',
+        'disk',
+        'mime_type',
+        'file_size',
+    ];
+}
