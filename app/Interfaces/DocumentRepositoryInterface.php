a8f3f7a9f75f939a7e11f549eb52896d06a2d0ec
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-15 02:57:39 +0500

    feat: Interface added for Document so we can implement in repository later

---
+++ app/Interfaces/DocumentRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Interfaces;
+use App\Models\Document;
+use Illuminate\Database\Eloquent\Collection;
+
+
+interface DocumentRepositoryInterface
+{
+    //
+    public function getPublicDocuments(): Collection;
+    public function getPrivateDocument(): Collection;
+    public function find(int $id): ?Document;
+    public function create(array $data): Document;
+    public function delete(int $id): bool;
+}
